9c61b51083859a9e8fbf5e4589ec554e0b55c1b7
BaelfireNightshd <25194649+BaelfireNightshd@users.noreply.github.com>
2018-11-23 05:34:29 -0800

    Add first programs.

--- freshInstall.bat
+++ freshInstall.bat
@@ -1,3 +1,13 @@
 
 :: Install chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:: Start installing packages
+:: development stuff
+choco install git atom python -y
+
+::web browswers
+choco install googlechrome firefox -y
+
+::creative stuff
+choco install krita blender gimp -y
